0abd23bf3f7745312a6f05f03f4b091f17b13028
Mateusz Śledź <sledziu1508@gmail.com>
2020-04-02 13:09:25 +0200

    new2

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
+
+class HelloResp(BaseModel):
+    msg: str
+
+
 @app.get('/')
-def hello_world():
+def hello_world() ->str:
     return {'message': 'Hello World during the coronavirus pandemic!'}
+
+@app.get("/hello/{name}", response_model=HelloResp)
+async def read_item(name: str) -> str:
+    return HelloResp(msg=f"Hello {name}")
+
+@app.post('/method')
+def post_f() ->str:
+	return {"method": "POST"}
+
+@app.get('/method')
+def get_f() ->str:
+	return {"method": "GET"}
+
+@app.put('/method')
+def put_f() ->str:
+	return {"method": "PUT"}
+
+@app.delete('/method')
+def del_f() ->str:
+	return {"method": "DELETE"}
